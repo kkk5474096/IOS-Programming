94747d7d5cac3511ee85d3033993232c2327340c
kkk5474096 <kkk5474096@naver.com>
2021-02-04 14:39:11 +0900

    navigation & modal

--- IOS-Programming/SecondViewcontroller.swift
+++ IOS-Programming/SecondViewcontroller.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewcontroller.swift
 //  IOS-Programming
 //
 //  Created by 이강민 on 2021/02/02.
@@ -7,57 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondViewcontroller: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("ViewController의 view가 메모리에 로드 됨")
+        print("SecondViewController의 view가 메모리에 로드 됨")
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         // Do any additional setup after loading the view.
         
-        print("ViewController의 view가 화면에 보여질 예정")
+        print("SecondViewController의 view가 화면에 보여질 예정")
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         // Do any additional setup after loading the view.
         
-        print("ViewController의 view가 화면에 보여짐")
+        print("SecondViewController의 view가 화면에 보여짐")
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         // Do any additional setup after loading the view.
         
-        print("ViewController의 view가 화면에서 사라질 예정")
+        print("SecondViewController의 view가 화면에서 사라질 예정")
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         // Do any additional setup after loading the view.
         
-        print("ViewController의 view가 화면에서 사라짐")
+        print("SecondViewController의 view가 화면에서 사라짐")
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         // Do any additional setup after loading the view.
         
-        print("ViewController의 view가 subview를 레이아웃 하려함")
+        print("SecondViewController의 view가 subview를 레이아웃 하려함")
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         // Do any additional setup after loading the view.
         
-        print("ViewController의 view가 subview를 레이아웃 함")
+        print("SecondViewController의 view가 subview를 레이아웃 함")
+    }
+    @IBAction func popToPrev(){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func dismissModal() {
+        self.dismiss(animated: true, completion: nil)
     }
 
-    
-}
+    /*
+    // MARK: - Navigation
 
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
