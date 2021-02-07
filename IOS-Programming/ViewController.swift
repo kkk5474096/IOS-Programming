023162029fdfa09733f9b7bdfa9373fa6f452fe2
kkk5474096 <kkk5474096@naver.com>
2021-02-07 15:47:00 +0900

    Gesture Recognizer 활용

--- IOS-Programming/ViewController.swift
+++ IOS-Programming/ViewController.swift
@@ -7,42 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var dateLabel: UILabel!
-    let dateFormatter: DateFormatter = {
-        let formatter: DateFormatter = DateFormatter()
-        formatter.dateStyle = .medium
-        formatter.timeStyle = .medium
-        //formatter.dateFormat = "yyyy/MM/dd hh:mm:ss" 우리가 원하는 형식으로도 가능
-        return formatter
-    }()
+//    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+//        self.view.endEditing(true)
+//    }
     
-    @IBAction func didDatePickerValueChanged(_ sender: UIDatePicker) {
-        print("value change")
-        
-        let date: Date = sender.date
-        //let date: Date = self.datePicker.date  outlet으로 선언된상황에서는 이것도 가능
-        let dateString: String = self.dateFormatter.string(from: date)
-        
-        self.dateLabel.text = dateString
-        
-    }
-    
-//    @IBOutlet weak var nameField: UITextField!
-//    @IBOutlet weak var ageField: UITextField!
-//    
-//    @IBAction func touchUpsetButton(_ sender: UIButton) {
-//        UserInformation.shared.name = nameField.text
-//        UserInformation.shared.age = ageField.text
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.datePicker.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)
+//        let tapGesture: UITapGestureRecognizer =
+//            UITapGestureRecognizer(target: self, action: #selector(self.tapView(_:)))
+//
+//        self.view.addGestureRecognizer(tapGesture)
+        // 코드로 탭제스터 작성
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
+        tapGesture.delegate = self
+        
+        self.view.addGestureRecognizer(tapGesture)
+        
+     }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        self.view.endEditing(true)
+        return true
     }
     
     
